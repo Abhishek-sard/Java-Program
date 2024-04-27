09e21d9ae2eb0e27720f1da4696af165af92937f
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-27 08:21:28 +0545

    Add files via upload

---
+++ IfelseifTest.java
@@ -0,0 +1,13 @@
+public class IfelseifTest {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x > 10) {
+            System.out.println("i is greater than 10");
+        } else if (x < 10) {
+            System.out.println("i is less than 10");
+        } else {
+            System.out.println("is is 10");
+        }
+        System.out.println("After if else if ladder statement");
+    }
+}
