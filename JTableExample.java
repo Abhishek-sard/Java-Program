094939a5dcc567783477207a7282a6c1246fbf9b
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-04-27 08:21:04 +0545

    Add files via upload

---
+++ JTableExample.java
@@ -0,0 +1,14 @@
+// import javax.swing.JFrame;
+// import javax.swing.JScrollPane;
+// import javax.swing.JTable; incompleted code
+// public class JTableExample {
+//     JFrame f;
+//     JTable j;
+//     JTableExample(){
+//         f=new JFrame();
+//         f.setTitle("JTable Example");
+//         String[][] data= {
+
+//         }
+//     }
+// }
